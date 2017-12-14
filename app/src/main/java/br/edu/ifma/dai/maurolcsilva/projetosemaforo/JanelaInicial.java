a97a3075748b7f994b6a95c1a218188e5c2ca4fa
Mauro Silva <maurosilva@ifma.edu.br>
2017-12-14 19:50:56 -0200

    Commit Inicial

---
+++ app/src/main/java/br/edu/ifma/dai/maurolcsilva/projetosemaforo/JanelaInicial.java
@@ -0,0 +1,68 @@
+package br.edu.ifma.dai.maurolcsilva.projetosemaforo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class JanelaInicial extends AppCompatActivity implements LocationListener {
+
+    private TextView lblLatitude;
+    private TextView lblLongitude;
+    private double latitude;
+    private double longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_janela_inicial);
+        lblLatitude = findViewById(R.id.lblLatitudeResultado);
+        lblLongitude = findViewById(R.id.lblLongitudeResultado);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        this.latitude = location.getLatitude();
+        this.longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    public void buscarLocalizacao(View v){
+        lblLatitude.setText(String.valueOf(this.latitude));
+        lblLongitude.setText(String.valueOf(this.longitude));
+    }
+}
